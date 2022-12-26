c588b64e70bdcb239e0f5d8dc9ac665016f45421
Sameeranjoshi <joshisameeran17@gmail.com>
2022-12-26 01:22:27 -0700

    Commit

---
+++ files/script_omp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## declare an array variable
+#declare -a arr=("atb_ikj_unrollk2.c"  "atb_ikj_unrollk2_tilei32.c"  "atb_ikj_unrollk4.c"  "atb_par.c"  "atb_par_unroll.c")
+
+declare -a arr=( "atbt_jik_unrolli_par.c" "atbt_par.c" "atbt_jki_j_par.c" "atbt_kij_i_par.c" "atbt_kji_j_par.c" "atbt_par.c" "atbt_ikj_tileij_par.c")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+	filename=$(basename -- "$i")
+	extension="${filename##*.}"
+	filename="${filename%.*}"
+#   	gcc -O3 -fopenmp atb_main.c parallel_codes/$i -o exe/$filename.out
+	gcc -O3 -fopenmp atbt_main.c parallel_codes/$i -o exe/$filename.out
+#	bash exe/$filename.out > reports/$filename.txt
+ 	inputfilename="input.txt"
+# 	inputfilename="dummy_input.txt"
+
+	if [ -e reports/$filename.txt ]
+           then
+           	echo "Moving the previous file to reports/$filename.txt.old and please check if old data was useful else delete the old log file"
+                mv reports/$filename.txt reports/$filename.txt.old
+        fi
+
+	echo "$filename" >> reports/$filename.txt
+	 #Open the file
+	 while read -r line; do
+	     #process the line
+	     echo "Inputsizes: $line"
+	     #Pass the line as input to another script
+	     	    echo "$line" | ./exe/$filename.out  >> reports/$filename.txt
+	 done< "$inputfilename"
+	echo "I am done with first test case named $(basename -- "$i")"
+done
+
